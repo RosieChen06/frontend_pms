1d35cd8ca3c97c257a2244664ce4e2536fb6ee03
RosieChen06 <katekyo.069@gmail.com>
2025-02-16 20:58:22 +0800

    Update VendorHomePage.jsx

--- src/pages/VendorHomePage.jsx
+++ src/pages/VendorHomePage.jsx
@@ -100,7 +100,7 @@ const VendorHomePage = () => {
         formData.append('statusInput', 'submit')
         console.log(dateSubmitFilterConfirm)
 
-        const {data} = await axios.post('http://localhost:4000/api/user/clientReadData',formData)
+        const {data} = await axios.post('https://backend-pms.vercel.app/api/user/clientReadData',formData)
         if(data.success){
             setClientData(data.clientData)
         }
